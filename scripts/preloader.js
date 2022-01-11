c752b1f80796b198ee11d273c0abdffd07b92d66
Minhaz <sharif35-3001@diu.edu.bd>
2022-01-11 15:06:02 +0600

    preloader changed success

--- scripts/preloader.js
+++ scripts/preloader.js
@@ -1,26 +1,6 @@
-$(document).ready(function () {
-	$(".preloader-ring").each(function () {
-		let text = $(this).text(),
-			textArr = text.split(""),
-			a = 0;
-		$(this).html("");
-		for (let i = 0; i <= 30; i++) {
-			if (!textArr[i] || textArr[i] == " ") {
-				textArr[i] = "";
-			}
-			$(this).append(
-				'<div class="preloader-sector" style="transform: rotateY(' +
-					a +
-					'deg) translateZ(160px)">' +
-					textArr[i] +
-					"</div>"
-			);
-			a = a + 12;
-		}
-	});
+$(function () {
 	$(window).on("load", function () {
 		$(".preloader-content").css("display", "none");
 	});
 	$(".preloader-content").css("display", "flex");
-
 });
